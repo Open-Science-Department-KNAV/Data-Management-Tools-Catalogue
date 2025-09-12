875c1a457c14914cd9f0860f40109610325d6478
Anna Soldanova <soldanova@lib.cas.cz>
2025-09-12 07:45:36 +0200

    Add files via upload
    All files for GH page

---
+++ data.js
@@ -0,0 +1,35 @@
+const lifecycleData = {
+  planning: {
+    title: "Planning",
+    description: "Preparing data management before a project or experiment, e.g., creating a Data Management Plan (DMP).",
+  },
+  collecting: {
+    title: "Collecting",
+    description: "Ensuring proper data collection, including metadata, documentation, and formats.",
+  },
+  organizing_collaborating: {
+    title: "Organizing & Collaborating",
+    description: "Collaboration and organization of data during the research process.",
+  },
+  processing: {
+    title: "Processing",
+    description: "Cleaning, transforming, and converting data into suitable formats for further use.",
+  },
+  analyzing: {
+    title: "Analyzing",
+    description: "Analyzing data to obtain results that answer research questions.",
+  },
+  storing_archiving: {
+    title: "Storing & Archiving",
+    description: "Ensuring long-term accessibility and integrity of data after project completion.",
+  },
+  sharing: {
+    title: "Sharing",
+    description: "Sharing data publicly or within teams, in compliance with legal and ethical requirements.",
+  },
+  reusing: {
+    title: "Reusing",
+    description: "Reusing existing data for new research purposes.",
+  }
+};
+
